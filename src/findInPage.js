2406ee466744a7b40a7c26b0e4b9c033379ada44
Bob Long <robertjflong@gmail.com>
2020-01-24 14:51:41 -0500

    tweaks

--- src/findInPage.js
+++ src/findInPage.js
@@ -96,6 +96,9 @@ class FindInPage extends Find{
     move(this[findBox], (0 - this[findBox].offsetHeight - 10), this.duration)
     return this[initialized] = true
   }
+  isVisible() {
+    return parseInt(document.querySelector(".find-box").style.top) > -20
+  }
   openFindWindow () {
     if (this[hasOpened]) {
       focusInput.call(this)
